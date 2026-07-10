ec41e14878804a163d13486878be5fe9c6a41344
Aditya Subramanian <adu101992@gmail.com>
2026-07-10 16:56:55 +0530

    Assigning collision layers for players, enemies, walls, and the 2 bullet types

--- enter-vaders/Scripts/level_gen.gd
+++ enter-vaders/Scripts/level_gen.gd
@@ -1,11 +1,20 @@
 extends Node
 
+const LEVEL_SCENE = preload("res://Scenes/level.tscn")
+@export var levelNode : Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	_load_level()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+func _load_level() -> void:
+	var level_instance = LEVEL_SCENE.instantiate()
+	level_instance.global_position.x = 0.0
+	level_instance.global_position.y = 0.0
+	add_child(level_instance)
+	levelNode = level_instance
